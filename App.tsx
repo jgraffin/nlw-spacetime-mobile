5535a1c14449d33cd3d2c98e15311586b9ee0dea
Julio Ferreira <julio.x.ferriera@questdiagnostics.com>
2023-05-19 21:01:58 -0300

    Add configurations, fonts

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  return (
+    <View className="bg-gray-900 flex-1 items-center justify-center">
+      <Text className='text-5xl font-bold text-gray-50 font-jam'>Hello World!</Text>
+      <StatusBar style="light" translucent />
+    </View>
+  );
+}
+
+
